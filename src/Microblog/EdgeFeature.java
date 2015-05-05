3e60d2e65fc47139915be4e64c662c7bf7830ff3
wangycthu <wangycthu@gmail.com>
2015-05-05 17:16:43 +0800

    implement getValueGradient

---
+++ src/Microblog/EdgeFeature.java
@@ -0,0 +1,5 @@
+package Microblog;
+
+public class EdgeFeature extends Feature {
+	String name = "EdgeFeature";
+}
--- src/Microblog/Feature.java
+++ src/Microblog/Feature.java
@@ -4,11 +4,3 @@ public class Feature {
 	String name = "Feature";
 	public double[] values;
 }
-
-class NodeFeature extends Feature {
-	String name = "NodeFeature";
-}
-
-class EdgeFeature extends Feature {
-	String name = "EdgeFeature";
-}
