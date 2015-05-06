71ef00093c351388d63431562aaec2626c90b100
dongchao <dongchao@xiaomi.com>
2015-05-06 19:23:02 +0900

    dev graph

--- src/Microblog/Thread.java
+++ src/Microblog/Thread.java
@@ -1,15 +1,69 @@
 package Microblog;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Thread {
-	public Node[] nodes;
-	public NodeFeature[] nodeFeatures;
-	public EdgeFeature[] edgeFeatures;
 	
+	private Node[] nodes;
+	private NodeFeature[] nodeFeatures;
+	private String[] nodeFeatureNames;
+	private EdgeFeature[] edgeFeatures;
+	private String[] edgeFeatureNames;
+	private int nodeCount;
 	
-	public Thread() {
 
+	public Thread(Node[] nodeList) {
+		this.nodes = nodeList;
+		this.nodeCount = nodeList.length;
+	}
+	
+	public void setNodeNames(String[] featureNames) throws ClassNotFoundException {
+		
+		this.nodeFeatureNames = featureNames;
+		Class featureClass = Feature.class;
+		for(int i=0; i<featureNames.length; i++) {
+			Class c = Class.forName(featureNames[i]);
+			Object featureObj = c.getInterfaces();
+			this.nodeFeatures[i] = (NodeFeature)featureObj;
+		}
+	}
+	
+	public void setEdgeFeatures(String[] featureNames) throws ClassNotFoundException {
+		this.edgeFeatureNames = featureNames;
+		Class featureClass = Feature.class;
+		for(int i=0; i<featureNames.length; i++) {
+			
+			Class c = Class.forName(featureNames[i]);
+			Object featureObj = c.getInterfaces();
+			this.edgeFeatures[i] = (EdgeFeature)featureObj;
+		}
+	}
+	
+	public void extractNodeFeatures() {
+		for(int i=0; i<this.nodeFeatures.length; i++) {
+			
+			this.nodeFeatures[i].extract(this.nodes);
+		}
+	}
+	
+	public void extractEdgeFeatures() {
+		for(int i=0; i<this.edgeFeatures.length; i++) {
+			
+			this.edgeFeautures[i].extract(this.nodes);
+		}
+	}
+	
+	public void extractFeatures() {
+		
+	}
+	public void getInstance() {
+		
+		
+	}
+	public void getLabel() {
+		
 	}
 }
---
+++ src/Microblog/Utils.java
@@ -0,0 +1,28 @@
+package Microblog;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class Utils {
+
+	
+	public Node[] readFile(String filePath) throws IOException {
+		
+		
+		
+		List<String> lines = Files.readAllLines(Paths.get(filePath), 
+				Charset.defaultCharset() );
+		Node[] nodes;
+		for(int i=0; i<lines.size(); i++) {
+			
+			JSONObject nodeJSON = new JSONObject(lines.get(i));
+			nodes.add(Node(nodeJSON));
+		}
+	}
+}
