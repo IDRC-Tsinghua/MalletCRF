74d7e48d8af21b0b56733a4fe1a7c60290cea8d9
dongchao <dongchao@xiaomi.com>
2015-05-12 16:06:23 +0900

    dev

--- src/Inference/GraphBuilder.java
+++ src/Inference/GraphBuilder.java
@@ -1,69 +1,32 @@
 package Inference;
 
+import Microblog.Node;
 import Microblog.Thread;
+import cc.mallet.grmm.inference.Inferencer;
+import cc.mallet.grmm.inference.JunctionTreeInferencer;
+import cc.mallet.grmm.types.Factor;
 import cc.mallet.grmm.types.FactorGraph;
-import cc.mallet.grmm.types.Variable;
-import Microblog.Thread;
-import Inference.FactorTable;
-import Utils.Constant;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class GraphBuilder {
-	public FactorGraph build(Thread[] threads, double[] params, 
-			int nodeFeatureNum, int EdgeFeatureNum) {
-		
-		Variable[] allVars = new Variable[threads.length * (1 + nodeFeatureNum)];
-		
-		
-		for (int i = 0; i < allVars.length; i++)
-			allVars[i] = new Variable(3);
-		FactorGraph graph = new FactorGraph (allVars);
-		
-		return graph;
+
+	public FactorGraph build(FactorTable factorTable, Thread thread) {
+
+		FactorGraph mdl = new FactorGraph();
+		for (Node node: thread.nodes) {
+
+            for (Factor nodeFactor : factorTable.nodeFeatureFactor) {
+                mdl.addFactor(nodeFactor);
+            }
+            for (Factor edgeFactor : factorTable.edgeFeatureFactor) {
+                mdl.addFactor(edgeFactor);
+            }
+		}
+        return mdl;
 	}
 
-	public FactorTable trainFactor(Thread[] threads, double[] params,
-                                   int nodeFeatureNum, int EdgeFeatureNum) {
+    public void inference(FactorGraph mdl) {
 
-        FactorTable factorTable = new FactorTable();
-
-        for (Thread thread: threads) {
-
-        }
+        Inferencer inf = new JunctionTreeInferencer();
+        inf.computeMarginals(mdl);
     }
-	
-	public FactorGraph buildOneGraph(Thread thread, double[] params, 
-			int nodeFeatureNum, int EdgeFeatureNum) throws NoSuchMethodException,
-			IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException {
-		
-		Variable[] allVars = new Variable[thread.getNodes().size() * 
-		                                (1+ nodeFeatureNum)];
-		FactorGraph graph = new FactorGraph(allVars);
-
-		for(int i=0; i<thread.nodeFeatureNames.length; i++) {
-
-			String nodeFeatureName = thread.getNodeFeatureNames()[i];
-			String featureFactorName = Constant.featureFactorMap.get(nodeFeatureName);
-
-			Class cls = FactorTable.class;
-			Object obj = cls.newInstance();
-			// Method method = FactorTable.class.getMethod(featureFactorName, FactorGraph.class);
-			Method method = cls.getDeclaredMethod(featureFactorName, FactorTable.class);
-			method.invoke(obj, graph);
-		}
-
-		for(int j=0; j<thread.edgeFeatureNames.length; j++) {
-
-			String edgeFeatureName = thread.getEdgeFeatureNames()[j];
-			String edgeFactorName = Constant.featureFactorMap.get(edgeFeatureName);
-			Class cls = FactorTable.class;
-			Object obj = cls.newInstance();
-			// Method method = FactorTable.class.getMethod(featureFactorName, FactorGraph.class);
-			Method method = cls.getDeclaredMethod(edgeFactorName, FactorTable.class);
-			method.invoke(obj, graph);
-		}
-		return graph;
-	}
 }
